4901d6cb3928203ac89632ef2e66e6cda86cfe83
Igor Stcherbina <orel1939@mail.ru>
2017-06-12 20:55:49 +0000

    One store for app menu.

---
+++ app-menu.js
@@ -0,0 +1,22 @@
+
+module.exports.add = Menu => {
+	// Create the Application's main menu
+	const template = [{
+		label: "Application",
+		submenu: [
+			{ label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+		]}, {
+		label: "Edit",
+		submenu: [
+			{ label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+			{ label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+			{ type: "separator" },
+			{ label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+			{ label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+			{ label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+			{ label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+		]}
+	];
+
+	Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+}
--- index-app.js
+++ index-app.js
@@ -19,26 +19,6 @@ app.on('ready', () => {
 				app.quit();
 			});
 
-			// Create the Application's main menu
-			const template = [{
-				label: "Application",
-				submenu: [
-					{ label: "About Application", selector: "orderFrontStandardAboutPanel:" },
-					{ type: "separator" },
-					{ label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
-				]}, {
-				label: "Edit",
-				submenu: [
-					{ label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
-					{ label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
-					{ type: "separator" },
-					{ label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
-					{ label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
-					{ label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
-					{ label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
-				]}
-			];
-
-			Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+			require('./app-menu').add(Menu);
 		});
 });
